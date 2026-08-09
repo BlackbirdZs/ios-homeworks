3247fe987588e567c21a9d0a9c9151ecc748039f
Acronym <clearsky61@gmail.com>
2026-08-09 20:48:56 +0300

    Final fixes for profile view

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,6 +9,9 @@ import Foundation
 import UIKit
 
 class ProfileHeaderView: UIView {
+    
+    private var statusText: String = ""
+
     lazy var avatar: UIImageView = {
         let avatar = UIImageView()
         avatar.layer.cornerRadius = 50
@@ -26,7 +29,7 @@ class ProfileHeaderView: UIView {
         nameLabel.font = UIFont.boldSystemFont(ofSize: 18)
         return nameLabel
     }()
-
+    
     lazy var statusLabel: UILabel = {
         let statusLabel = UILabel()
         statusLabel.text = "Waiting for something..."
@@ -34,10 +37,12 @@ class ProfileHeaderView: UIView {
         statusLabel.font = UIFont.systemFont(ofSize: 14)
         return statusLabel
     }()
+    
+    
 
     lazy var statusButton: UIButton = {
         let statusButton = UIButton()
-        statusButton.setTitle("Show status", for: .normal)
+        statusButton.setTitle("Set status", for: .normal)
         statusButton.setTitleColor(.white, for: .normal)
         statusButton.backgroundColor = .systemBlue
         statusButton.layer.cornerRadius = 4
@@ -51,12 +56,33 @@ class ProfileHeaderView: UIView {
         return statusButton
     }()
     
+    lazy var statusTextField: UITextField = {
+        let statusTextField = UITextField()
+        statusTextField.layer.borderColor = UIColor.black.cgColor
+        statusTextField.layer.cornerRadius = 12
+        statusTextField.layer.borderWidth = 1
+        statusTextField.backgroundColor = .white
+        statusTextField.placeholder = "Tell us how you're feeling today"
+        statusTextField.textColor = .black
+        statusTextField.font = UIFont.systemFont(ofSize: 15)
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 40))
+            statusTextField.leftView = paddingView
+            statusTextField.leftViewMode = .always
+        
+        statusTextField.addTarget(self, action: #selector(statusTextChanged(_ :)), for: .editingChanged)
+
+        
+        return statusTextField
+    }()
+    
     override init (frame: CGRect) {
         super.init(frame: frame)
         addSubview(avatar)
         addSubview(nameLabel)
         addSubview(statusLabel)
         addSubview(statusButton)
+        addSubview(statusTextField)
     }
 
     required init?(coder: NSCoder) {
@@ -72,26 +98,37 @@ class ProfileHeaderView: UIView {
             y: 27,
             width: bounds.width - avatar.frame.maxX - 16 - 16,
             height: 22
-            )
+        )
         
         statusLabel.frame = CGRect(
             x: nameLabel.frame.minX,
             y: nameLabel.frame.maxY + 6,
             width: nameLabel.frame.width,
             height: 20
-            )
+        )
         
+        statusTextField.frame = CGRect(
+            x: nameLabel.frame.minX,
+            y: statusLabel.frame.maxY + 8,
+            width: nameLabel.frame.width,
+            height: 40
+        )
         
         statusButton.frame = CGRect(
-            x:16,
-            y: avatar.frame.maxY + 34,
+            x: 16,
+            y: statusTextField.frame.maxY + 16,
             width: bounds.width - 32,
             height: 50
-            )
+        )
     }
     
     @objc func buttonPressed() {
-        print(statusLabel.text ?? "")
+        statusLabel.text = statusText
+        print(statusText)
+    }
+    
+    @objc func statusTextChanged(_ textField: UITextField) {
+        statusText = textField.text ?? ""
     }
     
 }
